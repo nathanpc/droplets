c2a822a65c30e794f2da47c6f1dbd0e39906bb5a
Nathan Campos <eeepc904@gmail.com>
2013-08-16 19:38:03 -0300

    Made a bunch of changes and forgot to commit them

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // main.js
 
 var onload = function () {
-	//
+	// TODO: Vertically venter the big weather thingy.
+	fix_scrolling_width();
+}
+
+var fix_scrolling_width = function () {
+	$("footer.forecast-container").css("width", $(window).width());
 }
